93c6619509c782f4f82e447f0857f95afe9290d4
DTasev <dimtasev@gmail.com>
2018-02-25 16:19:18 +0000

    Match status is now processed as title

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ class Match:
         self.status = status
         self.color = self.get_color(status)
         self.time = MatchTimes(time, time + datetime.timedelta(minutes=EVENT_DURATION_MINUTES))
-        self.name = f"{self.round} - {self.player_one} vs {self.player_two} - {self.status.capitalize()}"
+        self.name = f"{self.round} - {self.player_one} vs {self.player_two} - {self.status.replace('_', ' ').title()}"
 
     def __str__(self):
         return f"{self.player_one} versus {self.player_two} at {self.time.start.isoformat()}"
